a392b54c81c9c59ef1ec3010ac5a7a07c43ea7f7
Sort weste <00123216@uca.edu.sv>
2019-07-18 15:13:03 -0600

    Index added

--- src/main/kotlin/com/sort/capas/pruebakt/domain/Purchase.kt
+++ src/main/kotlin/com/sort/capas/pruebakt/domain/Purchase.kt
@@ -1,4 +1,16 @@
 package com.sort.capas.pruebakt.domain
 
-class Purchase {
-}
+import javax.persistence.*
+
+@Entity
+@Table(name="orden_compra")
+data class Purchase(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "c_orden_compra")
+        val id: Long,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "c_producto")
+        val products: Product
+)
